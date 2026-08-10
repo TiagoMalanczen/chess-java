5b7d1016ca1c3a1a83058dc4e4e8e389a544a629
Tiago Malanczen <malanczen13@gmail.com>
2026-08-10 15:48:08 -0300

    feat: adiciona ChessException e ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,11 +25,11 @@ public class ChessMatch {
 		return mat;
 	} 
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPostion(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 0));
-		
-		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
+		placeNewPiece('b', 6, new Rook(board, Color.BLACK));
 	}
 }
