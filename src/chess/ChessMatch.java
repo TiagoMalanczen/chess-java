207395e266020a301c23c5c5571249c1d590a95e
Tiago Malanczen <malanczen13@gmail.com>
2026-08-10 16:12:38 -0300

    refactor: melhora a impressao do tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,7 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -30,6 +30,10 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.BLACK));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        
 	}
 }
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,14 +1,15 @@
 package chess.pieces;
 
 import boardgame.Board;
-import boardgame.Piece;
+import chess.ChessPiece;
+import chess.Color;
 
-public class King extends Piece{
+public class King extends ChessPiece{
 
-	public King(Board board) {
-		super(board);
+	public King(Board board, Color color) {
+		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
