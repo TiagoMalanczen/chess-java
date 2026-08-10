df3cb6a4d43fbadf3136fe8827afc5acd12c447d
Tiago Malanczen <malanczen13@gmail.com>
2026-08-10 10:00:12 -0300

    feat: adiciona classe position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+
+	private int coluna;
+	private int linha;
+	
+	public Position(int coluna, int linha) {
+		super();
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	@Override
+	public String toString() {
+		return "coluna: + " + coluna + " linha: " + linha;
+	}
+	
+	
+}
